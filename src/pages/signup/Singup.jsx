78cc04cc89f8e6cad2d5b99237486dd815c4bfb9
sangmin827 <sangmin1243@naver.com>
2025-09-22 20:16:12 +0900

    + 회원가입 페이지 작성
     - 페이지.jsx만 만들었습니다

---
+++ src/pages/signup/Singup.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Singup = () => {
+  return (
+    <>
+      <header>
+        <a href="/">
+          <img src="../../pubilc/icon+logo.svg"/>
+        </a>
+      </header>
+    </>
+  );
+};
+
+export default Singup;
