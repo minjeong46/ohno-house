3ea46b1156b2c18509d6ff8f6f54582672fac5da
sangmin827 <sangmin1243@naver.com>
2025-09-22 20:14:57 +0900

    > 회원가입창 dom연결
    회원가입창 (SingUp) DOM을 연결했습니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/community/Home";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/community/Home';
+import Singup from './pages/signup/Singup';
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            {
-                index: true, // 기본페이지
-                element: <Home />,
-            },
-            {
-                path: "shop",
-                // 여기에 element
-            },
-            {
-                path: "login",
-                // 여기에 element
-            },
-        ],
-    },
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true, // 기본페이지
+        element: <Home />,
+      },
+      {
+        path: 'shop',
+        // 여기에 element
+      },
+      {
+        path: 'login',
+        // 여기에 element
+      },
+      {
+        path: 'sign',
+        element: <Singup />,
+      },
+    ],
+  },
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
