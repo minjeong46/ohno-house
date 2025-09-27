c70e2f62088d5a87c92f01389c5170de0d1f4d5b
sangmin827 <sangmin1243@naver.com>
2025-09-27 14:01:26 +0900

    + shopping 페이지 기본틀 작성
    기본틀 작성했습니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/community/Home";
+import Shop from './pages/shopping/Home';
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            {
-                index: true, // 기본페이지
-                element: <Home />,
-            },
-            {
-                path: "shop",
-                // 여기에 element
-            },
-            {
-                path: "login",
-                // 여기에 element
-            },
-        ],
-    },
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true, // 기본페이지
+        element: <Home />,
+      },
+      {
+        path: 'shop',
+        element: <Shop />,
+      },
+      {
+        path: 'login',
+        // 여기에 element
+      },
+    ],
+  },
 ]);
 
 function App() {
