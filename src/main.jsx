f6040f7ace228914a89ad4a3cad87e07751124bd
sangmin827 <sangmin1243@naver.com>
2025-09-29 23:48:51 +0900

    + REDUX 연결
    npm install @reduxjs/toolkit react-redux
    
     > 리덕스 인스톨 하여 연결해놨습니다

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+// 위 내용으로 통합
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* ✨ <Provider>로 <App />을 감싸고, store={store}를 전달 */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
