c02817ce54f76ac7415e4c4ad1d0a48a68f4aa2a
sangmin827 <sangmin1243@naver.com>
2025-09-30 00:32:44 +0900

    redux-persist 적용
    기존 redux 상태에서 redux-persist 설정과 middleware 설정을 추가하여 반영했습니다

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+//redux-persist 임포트
+import storage from 'redux-persist/lib/storage';
+
+//Persist 설정 정의 - user slice만 저장하도록 설정
+const persistConfig = {
+  key: 'root', 
+  storage,
+  whitelist: ['user'],
+};
+
+//userReducer를 persistConfig로 래핑
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
+  // 래핑된 리듀서 사용
   reducer: {
-    user: userReducer,
+    user: persistedUserReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
