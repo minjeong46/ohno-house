d1383224338ef573eccfc1d13630dcace4fbaf32
sangmin827 <sangmin1243@naver.com>
2025-09-30 00:26:31 +0900

    + redux-persist 설치 및 적용
    $ npm install redux-persist
    
    * 설치후 PersistGate로 감싸서, Local Storage에서 데이터를 로드할 때까지 UI 렌더링을 지연
    
     - 새로고침을 해도 로그인 정보가 그대로 유지되도록 했습니다

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = createRoot(document.getElementById('root'));
 // 위 내용으로 통합
@@ -17,7 +18,9 @@ root.render(
   <StrictMode>
     {/* ✨ <Provider>로 <App />을 감싸고, store={store}를 전달 */}
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
