caaf01e057ae6d16c8884cd17b1b2fb579e74601
sangmin827 <sangmin1243@naver.com>
2025-09-30 00:26:44 +0900

    변경사항 없음

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -16,7 +16,6 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
-    {/* ✨ <Provider>로 <App />을 감싸고, store={store}를 전달 */}
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
