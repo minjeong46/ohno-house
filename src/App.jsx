e8a7742fc7f5828af5e3462a25c1ff3645d71622
minjeong46 <eyou46@naver.com>
2025-10-01 10:53:25 +0900

    design: cart 디자인1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/community/Home";
+import Cart from "./pages/shoppingCart/Cart";
 
 const router = createBrowserRouter([
     {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
             {
                 path: "login",
                 // 여기에 element
+            },
+            {
+                path: "cart",
+                element: <Cart />,
             },
         ],
     },
---
+++ src/pages/shoppingCart/images.js
@@ -0,0 +1,10 @@
+import cartTodayGo from "../../assets/cart-today.png";
+import productImg1 from "../../assets/product-1.jpg";
+
+export const cartImg = {
+    cartTodayGo,
+};
+
+export const productImg = {
+    productImg1,
+};
