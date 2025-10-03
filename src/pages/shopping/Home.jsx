98a6f3c1f0038bf79f971ba3cf78121819e36748
sangmin827 <sangmin1243@naver.com>
2025-10-03 23:55:48 +0900

    - shopping Page에 impoort 제거
    쓸모없는 import를 제거했습니다

--- src/pages/shopping/Home.jsx
+++ src/pages/shopping/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Header from '../../components/Header';
 import HomeBanner from '../shopping/HomeBanner';
 import CategoryList from '../shopping/CategoryList';
-import TabList from '../shopping/TabList';
 import ProductSection from '../shopping/ProductSection';
 
 function Home() {
