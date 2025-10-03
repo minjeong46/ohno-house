1dc59460766eb2a3fd5bdcb0382c250334ec8ed4
sangmin827 <sangmin1243@naver.com>
2025-10-04 04:23:03 +0900

    * 모달 연결 수정
     >> 기존에 새로운 변수를 줘서 모달을 열어서 닫기가 안되는 오류를 이제 알아서 수정

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,35 @@
 import ScrapIcon from '../assets/community/scrap.svg';
 import ScrapIconFill from '../assets/community/scrap-fill.svg';
 import { useState } from 'react';
+import ProductDetailModal from './Modal/ProductDetailModal.jsx';
+import { useDispatch } from 'react-redux';
+import { openModal } from '../store/modalSlice.js';
 
 const ProductCard = ({ product }) => {
-  const [isScrapped, setIsScrapped] = useState(true); 
+  const [isScrapped, setIsScrapped] = useState(true);
+  const dispatch = useDispatch();
+
   const handleScrapClick = (e) => {
     e.stopPropagation();
-    e.preventDefault(); 
-    setIsScrapped(!isScrapped); 
-    };
-    
+    e.preventDefault();
+    setIsScrapped(!isScrapped);
+  };
+
+  const handleCardClick = () => {
+    dispatch(
+      openModal({
+        type: 'PRODUCT_DETAIL',
+        data: {
+          product: product,
+        },
+      })
+    );
+  };
+
   const sellingPrice = product.originalPrice * (1 - product.discountRate / 100);
 
   return (
-    <button className="w-full md:w-1/4 p-2">
+    <button className="w-full md:w-1/4 p-2" onClick={handleCardClick}>
       <div className="bg-white rounded-lg group">
         <div className="relative overflow-hidden aspect-square rounded">
           <img
@@ -21,9 +37,9 @@ const ProductCard = ({ product }) => {
             alt={product.name}
             className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
           />
-          <button
-            className="absolute bottom-1 right-1 p-1 duration-300 z-10" 
-            onClick={handleScrapClick} 
+          <div
+            className="absolute bottom-1 right-1 p-1 duration-300 z-10"
+            onClick={handleScrapClick}
             aria-label="스크랩"
           >
             <img
@@ -31,7 +47,7 @@ const ProductCard = ({ product }) => {
               alt="스크랩"
               className="w-5 h-6"
             />
-          </button>
+          </div>
         </div>
         <div className="p-3">
           <p className="text-xs text-gray-500 text-left">{product.brand}</p>
