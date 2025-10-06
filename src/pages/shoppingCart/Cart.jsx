7ae2b6ac52bf7c236a1ecab98b2be979081d1f56
sangmin827 <sangmin1243@naver.com>
2025-10-06 20:04:41 +0900

    전체 선택, 선택삭제 기능 구현
    - 전체선택시 전체 상품 선택되도록 구현
    
    - 선택삭제를 눌렀는데 선택한 상품이 없을시 선택한 상품이 없다는 모달 띄우게 만들었습니다
    
    - 선택삭제도 되도록 구현했습니다

--- src/pages/shoppingCart/Cart.jsx
+++ src/pages/shoppingCart/Cart.jsx
@@ -1,53 +1,104 @@
-import CartIcon from "../../assets/cart-icon1.svg?react";
-import { ChevronRightIcon } from "@heroicons/react/24/outline";
-import { CheckIcon } from "@heroicons/react/24/outline";
-import CartPayList from "./CartPayList";
-import CartCard from "./CartCard";
-import { useSelector } from "react-redux";
+import CartIcon from '../../assets/cart-icon1.svg?react';
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
+import { CheckIcon } from '@heroicons/react/24/outline';
+import CartPayList from './CartPayList';
+import CartCard from './CartCard';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleAllItems } from '../../store/userSlice';
+import { openModal } from '../../store/modalSlice';
+import { showToast } from '../../store/toastSlice';
+import { ShoppingBagIcon } from '@heroicons/react/24/outline';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
-    const { cart } = useSelector((state) => state.user);
+  const { cart } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const handleSelectAll = (e) => {
+    const selectAll = e.target.checked;
+    dispatch(toggleAllItems(selectAll));
+  };
+
+  const handleRemoveSelected = () => {
+    if (selectedCount === 0) {
+      dispatch(
+        showToast({
+          message: '선택한 상품이 없습니다.',
+          type: 'info',
+        })
+      );
+    } else {
+      dispatch(
+        openModal({
+          type: 'confirmRemoveSelected',
+          data: null,
+        })
+      );
+    }
+  };
+
+  if (isCartEmpty) {
     return (
-        <div className="max-w-[1256px] mx-auto pb-10 flex justify-center">
-            <div className="min-w-[814px]">
-                <a>
-                    <div className="py-2 px-4 mb-3 flex justify-between bg-[#F0F8FC] rounded-sm">
-                        <div className="flex items-center">
-                            <CartIcon className="w-6 h-6" />
-                            <span className="text-[13px] ml-2">
-                                나만의 패키지에 담으면 최저가 수준의 혜택이!
-                            </span>
-                        </div>
-                        <div className="flex items-center cursor-pointer">
-                            <span className="text-[13px]">바로가기</span>
-                            <ChevronRightIcon className="h-4 w-4 text-gray-800" />
-                        </div>
-                    </div>
-                </a>
-                <div className="py-2 px-4 flex justify-between rounded-sm border">
-                    <label className="inline-flex items-center cursor-pointer">
-                        <input type="checkbox" className="peer hidden" />
-                        <span className="w-5 h-5 rounded-md border border-gray-400 flex items-center justify-center peer-checked:bg-[#11A5FD] peer-checked:border-[#11A5FD]">
-                            <CheckIcon className="h-4 w-4 text-white" />
-                        </span>
-                        <span className="ml-2 text-sm text-gray-700">
-                            전체 선택
-                        </span>
-                    </label>
-                    <button className="py-[2px] px-2 rounded-md hover:bg-[#ddd]">
-                        <span className="text-sm text-gray-700">선택 삭제</span>
-                    </button>
-                </div>
-                {
-                   cart && cart.map((item)=>{
-                        return <CartCard product={item.product} quantity={item.quantity} />
-                    })
-                }
-            </div>
-            <CartPayList />
         </div>
     );
+  }
+
+  return (
+    <div className="max-w-[1256px] mx-auto pb-10 flex justify-center">
+      <div className="min-w-[814px]">
+        <a>
+          <div className="py-2 px-4 mb-3 flex justify-between bg-[#F0F8FC] rounded-sm">
+            <div className="flex items-center">
+              <CartIcon className="w-6 h-6" />
+              <span className="text-[13px] ml-2">
+                나만의 패키지에 담으면 최저가 수준의 혜택이!
+              </span>
+            </div>
+            <button
+              className="flex items-center"
+              onClick={() => navigate('/shop')}
+            >
+              <span className="text-[13px]">바로가기</span>
+              <ChevronRightIcon className="h-4 w-4 text-gray-800" />
+            </button>
+          </div>
+        </a>
+        <div className="py-2 px-4 flex justify-between rounded-sm border">
+          <label className="inline-flex items-center cursor-pointer">
+            <input
+              type="checkbox"
+              className="peer hidden"
+              checked={isAllChecked}
+              onChange={handleSelectAll}
+            />
+            <span className="w-5 h-5 rounded-md border border-gray-400 flex items-center justify-center peer-checked:bg-[#11A5FD] peer-checked:border-[#11A5FD]">
+              <CheckIcon className="h-4 w-4 text-white" />
+            </span>
+            <span className="ml-2 text-sm text-gray-700">전체 선택</span>
+          </label>
+          <button
+            className="py-[2px] px-2 rounded-md hover:bg-[#ddd]"
+            onClick={handleRemoveSelected}
+          >
+            <span className="text-sm text-gray-700">선택 삭제</span>
+          </button>
+        </div>
+        {cart &&
+          cart.map((item) => {
+            return (
+              <CartCard
+                key={item.product.id}
+                product={item.product}
+                quantity={item.quantity}
+                isChecked={item.isChecked || false}
+              />
+            );
+          })}
+      </div>
+      <CartPayList />
+    </div>
+  );
 };
 
 export default Cart;
