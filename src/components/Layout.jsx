ff3e6e316aaeca554ccb0b25e6d00dfc90ef2b1f
sangmin827 <sangmin1243@naver.com>
2025-10-06 19:50:38 +0900

    + 삭제 모달연결
    선택 삭제, 제품 하나씩 삭제 모달이 띄워지게 만들었습니다

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 import CartAmountModal from "./Modal/CartAmountModal";
 import ProductDetailModal from "./Modal/ProductDetailModal";
 import CartSuccessModal from "./Modal/CartSuccessModal";
+import RemoveConfirmModal from './Modal/RemoveConfirmModal';
+import RemoveSelectedConfirmModal from './Modal/RemoveSelectedConfirmModal';
 import { useSelector } from "react-redux";
 import Toast from "./Toast";
 
@@ -14,25 +16,28 @@ const Layout = () => {
     const { isOpen, modalType, data } = useSelector((state) => state.modal);
 
     return (
-        <div className="w-screen min-h-screen relative flex flex-col">
-            <Header setIsSidebar={setIsSidebar} />
-            <main className="flex-1 max-w-[1256px] mx-auto my-7 ">
-                <Outlet />
-            </main>
-            <Footer />
-            {isSidebar && <Sidebar setIsSidebar={setIsSidebar} />}
-            {isOpen && modalType === "cartAmount" && (
-                <CartAmountModal
-                    product={data.product}
-                    quantity={data.quantity}
-                />
-            )}
-            {isOpen && modalType === "PRODUCT_DETAIL" && data?.product && (
-                <ProductDetailModal product={data.product} />
-            )}
-            {isOpen && modalType === "cartSuccess" && <CartSuccessModal />}
-            <Toast />
-        </div>
+      <div className="w-screen min-h-screen relative flex flex-col">
+        <Header setIsSidebar={setIsSidebar} />
+        <main className="flex-1 max-w-[1256px] mx-auto my-7 ">
+          <Outlet />
+        </main>
+        <Footer />
+        {isSidebar && <Sidebar setIsSidebar={setIsSidebar} />}
+        {isOpen && modalType === 'cartAmount' && (
+          <CartAmountModal product={data.product} quantity={data.quantity} />
+        )}
+        {isOpen && modalType === 'PRODUCT_DETAIL' && data?.product && (
+          <ProductDetailModal product={data.product} />
+        )}
+        {isOpen && modalType === 'cartSuccess' && <CartSuccessModal />}
+        {isOpen && modalType === 'confirmRemove' && data?.product && (
+          <RemoveConfirmModal product={data.product} quantity={data.quantity} />
+        )}
+        {isOpen && modalType === 'confirmRemoveSelected' && (
+          <RemoveSelectedConfirmModal />
+        )}
+        <Toast />
+      </div>
     );
 };
 
