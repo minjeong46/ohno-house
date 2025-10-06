89df53a26b8a71085a26385d3cf5b762e5863e9f
sangmin827 <sangmin1243@naver.com>
2025-10-06 19:51:55 +0900

    * Toast 수정
    toast 내용 수정했습니다

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,29 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react'; 
 import { useDispatch, useSelector } from 'react-redux';
-import { hideToast } from '../store/toastSlice';
+import { hideToast, clearToastMessage } from '../store/toastSlice';
 
 const Toast = () => {
   const dispatch = useDispatch();
   const { message, isVisible, type } = useSelector((state) => state.toast);
 
+  const [show, setShow] = useState(false);
+
   useEffect(() => {
     let timer;
+    let transitionTimer;
+    let mountTimer;
+
     if (isVisible) {
+      mountTimer = setTimeout(() => {
+        setShow(true);
+      }, 10);
+
       timer = setTimeout(() => {
         dispatch(hideToast());
       }, 3000);
     }
-    return () => clearTimeout(timer);
-  }, [isVisible, dispatch]);
-  if (!isVisible && !message) return null;
-  const baseStyle =
-    'fixed left-1/2 top-1/2 transform p-4 rounded-lg text-white shadow-xl z-[9999] -translate-x-1/2 -translate-y-1/2';
+    else if (!isVisible && message) {
+      setShow(false);
 
-  const typeStyle = type === 'error' ? 'bg-red-500' : 'bg-green-500';
+      transitionTimer = setTimeout(() => {
+        dispatch(clearToastMessage());
+      }, 300);
+    }
+
+    return () => {
+      clearTimeout(mountTimer);
+      clearTimeout(timer);
+      clearTimeout(transitionTimer);
+    };
+  }, [isVisible, message, dispatch]);
+
+  if (!message) return null;
+
+  const baseStyle =
+    'fixed left-1/2 bottom-10 transform px-5 py-3 rounded-sm text-xs text-white shadow-2xl z-[9999] -translate-x-1/2 transition-all duration-300';
+
+  const typeStyle =
+    type === 'error'
+      ? 'bg-red-500'
+      : type === 'success'
+      ? 'bg-green-500'
+      : 'bg-gray-800/90';
+
+  const visibilityStyle = show
+    ? 'opacity-100'
+    : 'opacity-0';
 
   return (
-    <div className={`${baseStyle} ${typeStyle}`}>
-      {message}
+    <div className={`${baseStyle} ${typeStyle} ${visibilityStyle}`}>
+       {message}
     </div>
   );
 };
